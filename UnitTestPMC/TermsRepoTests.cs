ef0bb687a5fb58ff46d968d71ffd8cbbdd9d29a7
NerdL8y <dlr36@students.uwf.edu>
2021-02-28 17:37:52 -0600

    Adding Unit Tests, or at least attempting to.

---
+++ UnitTestPMC/TermsRepoTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMC.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMC.Data.Tests
+{
+    [TestClass()]
+    public class TermsRepoTests
+    {
+        [TestMethod()]
+        public void TermsRepoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
