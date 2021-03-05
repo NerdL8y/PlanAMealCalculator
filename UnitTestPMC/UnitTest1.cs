f817e6ea0bedd086918eb9062273f8417124fd44
NerdL8y <dlr36@students.uwf.edu>
2021-03-05 16:48:45 -0600

    Tried

--- UnitTestPMC/UnitTest1.cs
+++ UnitTestPMC/UnitTest1.cs
@@ -1,4 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PMC.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace UnitTestPMC
 {
@@ -8,6 +13,24 @@ namespace UnitTestPMC
         [TestMethod]
         public void TestMethod1()
         {
+            // arrange
+            string testUserName = "dlroy";
+            string testPassword = "1234567";
+
+            Data.RepoFactory repoFactory = new Data.RepoFactory();
+            var sessionRepo = repoFactory.Get<Data.SessionInfoRepo>();
+            var currentSessionID = sessionRepo.Authenticate(testUserName, testPassword);
+
+            repoFactory.SessionId = currentSessionID.SsID;
+            var userRepo = repoFactory.Get<Data.UserRepo>();
+            var currentUser = userRepo.GetUserBySessionID(repoFactory.SessionId);
+            repoFactory.UserID = currentUser.UserID;
+
+            // act
+            var repo = repoFactory.Get<Data.TermsRepo>();
+            var singleTerm = repo.GetTermByTermID(1);
+            Assert.AreEqual("Bake", singleTerm.ToString());
+            //Assert.Fail();
         }
     }
 }
